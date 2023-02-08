26185abec1e465659e4b10cd3cce36fedfb07577
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-02-08 18:44:20 -0300

    Create a admin seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,23 @@
+import { hash } from "bcryptjs";
+import { randomUUID } from "node:crypto";
+
+import { createConnection } from "@shared/infra/typeorm";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = randomUUID();
+  const password = await hash("admin", 8);
+
+  connection.query(
+    `INSERT INTO USERS(id, name, email, password, is_admin, driver_license, created_at)
+      values('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'avb123','now()')
+    `
+  );
+
+  await connection.destroy();
+}
+
+create().then(() => {
+  console.log("User admin created");
+});

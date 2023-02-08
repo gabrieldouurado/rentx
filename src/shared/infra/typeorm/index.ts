60118a028d8c9b71bb26faffaa2ad4553fbd5ff1
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-02-08 18:13:22 -0300

    Create car controller and routes

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
@@ -12,7 +13,7 @@ const AppDataSource = new DataSource({
   username: "docker",
   password: "ignite",
   database: "rentalx",
-  entities: [Category, Specification, User],
+  entities: [Category, Specification, User, Car],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
 

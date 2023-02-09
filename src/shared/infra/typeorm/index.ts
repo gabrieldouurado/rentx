077672aa19b147e67c782d7078eae31a21fac78e
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-02-09 15:56:31 -0300

    Create a upload car images

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,7 @@ import { DataSource } from "typeorm";
 
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
@@ -13,7 +14,7 @@ const AppDataSource = new DataSource({
   username: "docker",
   password: "ignite",
   database: "rentalx",
-  entities: [Category, Specification, User, Car],
+  entities: [Category, Specification, User, Car, CarImage],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
 

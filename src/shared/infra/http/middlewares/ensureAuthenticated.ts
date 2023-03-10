5df21eda2e99ade81ec8cc5357606b41f60275e9
João Gabriel Dourado <joao.dourado96@gmail.com>
2023-03-10 15:13:31 -0300

    Create refresh token controller and refactoy ensure authenticated for use refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -3,7 +3,7 @@ import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
 import { AppError } from "@errors/AppError";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 
 interface IPayload {
   sub: string;
@@ -23,11 +23,17 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
+    const { sub: user_id } = verify(
+      token,
+      auth.secret_refresh_token
+    ) as IPayload;
 
-    const usersRepository = new UsersRepository();
+    const usersTokensRepository = new UsersTokensRepository();
 
-    const user = await usersRepository.findById(user_id);
+    const user = await usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError("User does not exists", 401);
